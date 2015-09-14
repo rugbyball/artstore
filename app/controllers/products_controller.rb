1aaf723523a6103911243e7e3201ec5ab04b7b68
James Tzeng <tzeng.icr915@gmail.com>
2015-09-15 00:44:12 +0800

    toolstore-v2 2015.9.14

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,27 @@
+class ProductsController < ApplicationController
+  #before_action :authenticate_user!
+
+  def index
+    #@products = Product.all
+    @products = Product.order("id DESC")
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    #current_cart.add_product_to_cart(@product)
+
+    if !current_cart.items.include?(@product)
+      current_cart.add_product_to_cart(@product)
+      flash[:notice] = "你已經成功將 #{@product.title} 加入購物車"
+    else
+      flash[:warning] = "你的購物車內已有此商品"
+    end
+
+    redirect_to :back
+  end
+
+end
---
+++ db/migrate/20150914140855_add_payment_method_to_order.rb
@@ -0,0 +1,4 @@
+class AddPaymentMethodToOrder < ActiveRecord::Migration
+  def change
+  end
+end
